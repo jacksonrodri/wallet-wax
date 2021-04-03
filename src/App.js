4fc66f6d6ee787f0b835c2cdde7545323e177d1f
harsimarriar96 <harsimarriar96@gmail.com>
2021-04-03 19:02:57 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import * as waxjs from '@waxio/waxjs/dist';
+import 'tailwindcss/tailwind.css';
 
 function App() {
+  const [userAccount, setUserAccount] = useState('');
+  const [publicKey, setPublicKey] = useState('');
+  const [error, setError] = useState('');
+  useEffect(() => {
+    autoLogin();
+  }, []);
+
+  const autoLogin = async () => {
+    const wax = new waxjs.WaxJS('https://wax.greymass.com', null, null, false);
+    const isAutoLoginAvailable = await wax.isAutoLoginAvailable();
+    if (isAutoLoginAvailable) {
+      let uAccount = wax.userAccount;
+      let pubKeys = wax.pubKeys;
+      setPublicKey(pubKeys[0]);
+      setUserAccount(uAccount);
+    } else {
+      setError('You are not logged In.');
+    }
+  };
+
+  const login = async () => {
+    const wax = new waxjs.WaxJS('https://wax.greymass.com', null, null, false);
+    try {
+      let uAccount = await wax.login();
+      let pubKeys = wax.pubKeys;
+      setPublicKey(pubKeys[0]);
+      setUserAccount(uAccount);
+      setUserAccount(uAccount);
+    } catch (e) {
+      setError('Failed to login, please try again.');
+    }
+  };
+
+  // const fetchAssets = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-screen bg-primary flex flex-col items-center justify-center">
+      <h1 className="text-center text-6xl text-hover mb-20 uppercase">
+        NFT Story Cards
+      </h1>
+
+      <div className="text-white text-center text-lg mb-12">
+        {publicKey && <p className="mb-4">PUBLIC KEY: {publicKey}</p>}
+        {userAccount && <p>USER ACCOUNT: {userAccount}</p>}
+      </div>
+
+      {!publicKey && (
+        <button
+          onClick={() => login()}
+          className="w-64 bg-secondary rounded-xl h-12 hover:bg-hover uppercase text-primary text-xl shadow-xl"
         >
-          Learn React
-        </a>
-      </header>
+          Login
+        </button>
+      )}
+      {publicKey && (
+        <button
+          onClick={() => setError('Coming Soon...')}
+          className="w-64 bg-secondary rounded-xl h-12 hover:bg-hover uppercase text-primary text-xl shadow-xl"
+        >
+          Authenticate
+        </button>
+      )}
+
+      {error && <p className="mt-12 text-lg text-white">{error}</p>}
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4C2C1A',
+        secondary: '#AB865D',
+        hover: '#F2D6AF',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
